b17cb0b4168506ca7ead67fe2cd02eb0382eeecd
Medic5700 <super_alias_the_first@hotmail.com>
2016-04-01 06:36:37 -0400

    Renamed ReadMes

--- Programming Syntax/Python3/Basic/math_temp.py
+++ Programming Syntax/Python3/Basic/math_temp.py
@@ -15,6 +15,11 @@
 
 '''
 
+print(pow(10,2))
+print(min(10,2))
+print(max(10,2))
+
+
 # https://docs.python.org/3.2/library/math.html
 import math
 print(math.e)
@@ -22,5 +27,5 @@ print(math.pi)
 print(math.ceil(5.5)) #floor
 print(math.log(100,10))
 print(math.pow(10,2))
-print(pow(10,2))
+
 print(math.sqrt(4))
