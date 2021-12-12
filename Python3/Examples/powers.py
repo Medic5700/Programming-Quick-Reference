a9576ccdc4f2043ca6d7efedc41e78c104acc0eb
Medic5700 <Medic5700@users.noreply.github.com>
2021-12-11 20:34:35 -0500

    minor

--- Python3/Examples/powers.py
+++ Python3/Examples/powers.py
@@ -1,2 +1,9 @@
-for i in range(0,21):
+'''
+Author: Medic5700
+
+A simple program that displays the powers of 2.
+An example with a focus on string formatting.
+'''
+
+for i in range(21):
     print("2^" + str(i) + "\t=\t" + str(2**i))
